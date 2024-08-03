22238e34c2ee47558d143c08ec56a264d6c243ed
bhargav yarlagadda <119490919+bhargav-yarlagadda@users.noreply.github.com>
2024-08-03 23:18:06 +0530

    final commit

--- src/config/Gemini.js
+++ src/config/Gemini.js
@@ -8,8 +8,8 @@ import {
   const API_KEY =  process.env.API_KEY ?? ""; 
   const MODEL_NAME =  process.env.MODEL_NAME?? "";
   async function runChat(prompt) {
-    const genAI = new GoogleGenerativeAI(API_KEY);
-    const model = genAI.getGenerativeModel({ model: MODEL_NAME });
+    const genAI = new GoogleGenerativeAI("AIzaSyD8urIbmj0aX7p3XRqTv_036Bo6YNKTlw8");
+    const model = genAI.getGenerativeModel({ model: "gemini-1.0-pro"});
   
     const generationConfig = {
       temperature: 0.9,
@@ -21,19 +21,19 @@ import {
     const safetySettings = [
       {
         category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
       },
       {
         category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
       },
       {
         category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
       },
       {
         category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
       },
     ];
   
